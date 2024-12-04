36f246d5b41381fb35bf77c5a1f4ccd2110b1044
minttkanyarat <kanyaratchandang@gmail.com>
2024-12-04 16:17:21 +0700

    Add files via upload

---
+++ 1form_29mint.py
@@ -0,0 +1,11 @@
+print("Welcome to kanyarat room")
+
+print("\n\nสวัสดีค้าบ กรุณากรอกข้อมูลของตนเองที่เกี่ยวข้องค้าบ ")
+gender = input ("เพศ")
+weight = input ("น้ำหนัก")
+height = input ("ส่วนสูง") 
+age = input ("อายุ")
+
+print("\n\nยินดีต้อนรับค้าบ พี่ๆเพื่อนๆน้องๆที่รักสุขภาพ ")
+print("เพศ", gender,"\nน้ำหนัก ", weight,"\nส่วนสูง ", height,"\nอายุ ", age)
+print("\n\nรักษาสุขภาพด้วยนะค้าบ เลิฟเลิฟ")
